f08a43110345ccaa63fbfc64c1ac1f3e03faa1af
Fabio Rossini Sluzala <fabio3rs@gmail.com>
2020-11-30 23:35:53 -0300

    About page improvements

--- SyncDetect/About.Designer.cs
+++ SyncDetect/About.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.abouttext = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // abouttext
@@ -36,31 +38,49 @@
             this.abouttext.AutoSize = true;
             this.abouttext.Dock = System.Windows.Forms.DockStyle.Fill;
             this.abouttext.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.abouttext.Location = new System.Drawing.Point(0, 0);
+            this.abouttext.Location = new System.Drawing.Point(3, 0);
             this.abouttext.Name = "abouttext";
-            this.abouttext.Size = new System.Drawing.Size(1173, 25);
+            this.abouttext.Size = new System.Drawing.Size(579, 426);
             this.abouttext.TabIndex = 0;
             this.abouttext.Text = "Programa desenvolvido por Fábio Rossini Sluzala. Endereço do código fonte: https:" +
     "//github.com/Fabio3rs/SFTPSyncFiles";
             this.abouttext.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.abouttext.Click += new System.EventHandler(this.abouttext_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.abouttext, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(585, 426);
+            this.tableLayoutPanel1.TabIndex = 1;
             // 
             // About
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(552, 342);
-            this.Controls.Add(this.abouttext);
+            this.ClientSize = new System.Drawing.Size(585, 426);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "About";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sobre";
             this.Load += new System.EventHandler(this.About_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label abouttext;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
--- SyncDetect/About.cs
+++ SyncDetect/About.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace SyncDetect
 {
@@ -20,6 +22,42 @@ namespace SyncDetect
         private void About_Load(object sender, EventArgs e)
         {
             abouttext.Text = "Programa desenvolvido por\nFábio Rossini Sluzala.\n\nEndereço do código fonte\ne mais informações:\nhttps://github.com/Fabio3rs/SFTPSyncFiles";
+        }
+
+        // https://brockallen.com/2016/09/24/process-start-for-urls-on-net-core/
+        // https://stackoverflow.com/questions/4580263/how-to-open-in-default-browser-in-c-sharp
+        private void OpenUrl(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private void abouttext_Click(object sender, EventArgs e)
+        {
+            OpenUrl("https://github.com/Fabio3rs/SFTPSyncFiles");
         }
     }
 }
